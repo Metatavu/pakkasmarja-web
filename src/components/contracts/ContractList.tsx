3ff104404f25cdf6778b9bb588c2ab1b58a18c5b
julienbergstrom <julien.bergstrom@metatavu.fi>
2019-04-05 15:47:40 +0300

    hotfix

--- src/components/contracts/ContractList.tsx
+++ src/components/contracts/ContractList.tsx
@@ -69,7 +69,7 @@ class ContractList extends React.Component<Props, State> {
     if (!this.props.keycloak || !this.props.keycloak.token) {
       return;
     }
-    
+
     this.setState({ contractsLoading: true });
 
     const contractsService = await Api.getContractsService(this.props.keycloak.token);
@@ -95,7 +95,7 @@ class ContractList extends React.Component<Props, State> {
         contract: freshContract,
         itemGroup: itemGroup
       });
-      
+
       this.setState({ freshContracts: freshContractsState });
     });
 
@@ -138,14 +138,11 @@ class ContractList extends React.Component<Props, State> {
   public render() {
     return (
       <BasicLayout>
-        {
-          "rolecheck?" === "rolecheck?" &&
-          <Segment>
-            <Button as={Link} to={`contractManagement`} inverted color="red">
-              Sopimusten hallinta
+        <Segment>
+          <Button as={Link} to={`contractManagement`} inverted color="red">
+            Sopimusten hallinta
             </Button>
-          </Segment>
-        }
+        </Segment>
         <Segment>
           <Header>
             {strings.frozenContracts}
