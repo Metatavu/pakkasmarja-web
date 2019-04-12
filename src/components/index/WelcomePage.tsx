76c93e39e567a4ad8898cc99f8f4506f843881de
Ville Koivukangas <ville.koivukangas12@gmail.com>
2019-04-12 11:56:04 +0300

    Added authentication refresh to every route

---
+++ src/components/index/WelcomePage.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import {
+  Grid,
+  Header
+} from "semantic-ui-react";
+import strings from "../../localization/strings";
+import BasicLayout from "../generic/BasicLayout";
+import "../../styles/common.scss";
+
+/**
+ * Class for welcome page
+ */
+export default class WelcomePage extends React.Component {
+
+  /**
+   * Render method
+   */
+  public render() {
+    return (
+      <BasicLayout>
+        <Grid centered>
+          <Grid.Row>
+            <Header className="welcomeHeader" textAlign="center" >{strings.welcome}</Header>
+          </Grid.Row>
+        </Grid>
+      </BasicLayout>
+    );
+  }
+}
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,43 +2,69 @@ import * as React from "react";
 import * as Keycloak from 'keycloak-js';
 import {
   Grid,
-  Header
+  Loader
 } from "semantic-ui-react";
 import * as actions from "../../actions";
 import BasicLayout from "../generic/BasicLayout";
 import { Redirect } from "react-router";
-import strings from "../../localization/strings";
 import { StoreState } from "src/types";
 import { Dispatch } from "redux";
 import { KeycloakInstance } from "keycloak-js";
 import { connect } from "react-redux";
 
-export interface Props {
-  authenticated: boolean,
-  keycloak?: Keycloak.KeycloakInstance,
-  onLogin?: (keycloak: Keycloak.KeycloakInstance, authenticated: boolean) => void
+/**
+ * Interface for component props
+ */
+interface Props {
+  authenticated: boolean;
+  keycloak?: Keycloak.KeycloakInstance;
+  onLogin?: (keycloak: Keycloak.KeycloakInstance, authenticated: boolean) => void;
+  redirectPath?: string;
 }
 
+/**
+ * Interface for component state
+ */
 interface State {
-  loadingRealms: boolean
+  loadingRealms: boolean;
+  redirectPath: string;
 }
 
+/**
+ * Class for component login
+ */
 class Login extends React.Component<Props, State> {
 
+  /**
+   * Constructor
+   * 
+   * @param props props
+   */
   constructor(props: Props) {
     super(props);
     this.state = {
-      loadingRealms: false
+      loadingRealms: false,
+      redirectPath: "/"
     };
   }
 
-  componentDidMount() {
+  /**
+   * Component did mount life-cycle event
+   */
+  public componentDidMount() {
+    if (this.props.redirectPath) {
+      this.setState({ redirectPath: this.props.redirectPath });
+    }
+
     if (!this.props.authenticated) {
       this.doLogin();
     }
   }
 
-  doLogin() {
+  /**
+   * Do login
+   */
+  private doLogin() {
     const kcConf = {
       "realm": process.env.REACT_APP_REALM,
       "url": process.env.REACT_APP_AUTH_SERVER_URL,
@@ -47,30 +73,33 @@ class Login extends React.Component<Props, State> {
     
     const keycloak = Keycloak(kcConf);
     keycloak.init({onLoad: "login-required"}).success((authenticated) => {
-      console.log(authenticated);
       this.props.onLogin && this.props.onLogin(keycloak, authenticated);
     });
   }
 
-  render() {
+  /**
+   * Render method
+   */
+  public render() {
     return (
       <BasicLayout>
         { this.props.authenticated ? (
-          <div>
-            <Redirect to="/" />
-          </div>
+          <Redirect to={this.state.redirectPath} />
         ) : (
           <Grid centered>
-              <Grid.Row>
-                <Header textAlign="center" >{strings.redirectingTokeycloak}</Header>
-              </Grid.Row>
-            </Grid>
+            <Loader active size="medium" />
+          </Grid>
         )}
       </BasicLayout>
     );
   }
 }
 
+/**
+ * Redux mapper for mapping store state to component props
+ * 
+ * @param state store state
+ */
 export function mapStateToProps(state: StoreState) {
   return {
     authenticated: state.authenticated,
@@ -78,6 +107,11 @@ export function mapStateToProps(state: StoreState) {
   }
 }
 
+/**
+ * Redux mapper for mapping component dispatches 
+ * 
+ * @param dispatch dispatch method
+ */
 export function mapDispatchToProps(dispatch: Dispatch<actions.AppAction>) {
   return {
     onLogin: (keycloak: KeycloakInstance, authenticated: boolean) => dispatch(actions.userLogin(keycloak, authenticated))
