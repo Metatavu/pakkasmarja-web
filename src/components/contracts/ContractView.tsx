c3a55fc304e118efaa20fd704da510fb105269a3
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2020-05-15 17:35:55 +0300

    Added return lines to tsdocs

--- src/components/contracts/ContractView.tsx
+++ src/components/contracts/ContractView.tsx
@@ -438,6 +438,7 @@ class ContractView extends React.Component<Props, State> {
   /**
    * Returns true if all fields of area detail values are filled
    * @param areaDetailValues area detail values
+   * @returns true if all fields are filled, otherwise false
    */
   private allFieldsFilled = (areaDetailValues: AreaDetail[]): boolean => {
     for (const areaDetail of areaDetailValues) {
@@ -453,6 +454,7 @@ class ContractView extends React.Component<Props, State> {
   /**
    * Returns if contract proposed quantity is at least the total amount calculated from area details
    * @param totalAmount total amount calculated from area details
+   * @returns true is quantity is at least the total amount, otherwise false
    */
   private isValidContractMinimumAmount = (totalAmount: number): boolean => {
     const { proposedQuantity } = this.state.contractData;
@@ -463,6 +465,7 @@ class ContractView extends React.Component<Props, State> {
    * Returns total amount from area detail values
    * @param areaDetailValues area detail values
    * @param minimumProfit minimum profit, if predefined in contract
+   * @returns total amount as number
    */
   private calculateTotalAmount = (areaDetailValues: AreaDetail[], minimumProfit?: number): number => {
     const hasItems = areaDetailValues.length > 0;
