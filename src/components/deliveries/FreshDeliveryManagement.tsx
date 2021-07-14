a673d9a3912d26df0242ac44bc1022f520793e24
atte <atte.marttinen@metatavu.fi>
2021-07-14 14:46:43 +0300

    changed free field table to show daily data

--- src/components/deliveries/FreshDeliveryManagement.tsx
+++ src/components/deliveries/FreshDeliveryManagement.tsx
@@ -289,7 +289,7 @@ class FreshDeliveryManagement extends React.Component<Props, State> {
       eveningSalesForecastDataSheet: undefined
     });
 
-    const storageDataSheet = await this.prepareDataSheet(await this.findOrCreateDataSheet(`fresh-storage`));
+    const storageDataSheet = await this.prepareDataSheet(await this.findOrCreateDataSheet(`fresh-storage-${formattedDate}`));
     const morningSalesForecastDataSheet = await this.prepareDataSheet(await this.findOrCreateDataSheet(`fresh-morning-sales-forecast-${formattedDate}`));
     const eveningSalesForecastDataSheet = await this.prepareDataSheet(await this.findOrCreateDataSheet(`fresh-evening-sales-forecast-${formattedDate}`));
 
