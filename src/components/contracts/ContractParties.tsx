3c69c54ef0d0b3befbed6992f84c82cc9e6818b3
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2022-05-24 10:58:05 +0300

    Added SAP ID to farmer info in contract parties

--- src/components/contracts/ContractParties.tsx
+++ src/components/contracts/ContractParties.tsx
@@ -14,66 +14,51 @@ interface Props {
 }
 
 /**
- * Interface for component State
- */
-interface State {
-}
-
-/**
  * Class for contract parties component
  */
-export default class ContractParties extends React.Component<Props, State> {
-
-  /**
-   * Constructor
-   *
-   * @param props props
-   */
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-    };
-  }
+export default class ContractParties extends React.Component<Props> {
 
   /**
    * Render method
    */
-  public render() {
-    if (!this.props.contact) {
+  public render = () => {
+    const { contact, companyName, companyBusinessId } = this.props;
+
+    if (!contact) {
       return <div></div>;
     }
 
-    const farmerCompanyName = this.props.contact.companyName;
-    const farmerFirstName = this.props.contact.firstName;
-    const farmerLastName = this.props.contact.lastName;
+    const farmerDisplayName = contact.companyName ?? `${contact.firstName} ${contact.lastName}`;
 
     return (
       <div className="contract-white-container">
-          <Header as='h2'>
-            {strings.parties}
-         </Header>
+        <Header as="h2">
+          { strings.parties }
+        </Header>
         <Grid>
           <Grid.Row columns="2">
             <Grid.Column>
-              <p className="font-bold">{strings.farmer}</p>
+              <p className="font-bold">
+                { strings.farmer }
+              </p>
             </Grid.Column>
             <Grid.Column>
-              <p className="font-bold">{strings.company}</p>
+              <p className="font-bold">
+                { strings.company }
+              </p>
             </Grid.Column>
           </Grid.Row>
-          <Grid.Row columns="2" style={{paddingTop:0}}>
+          <Grid.Row
+            columns="2"
+            style={{ paddingTop: 0 }}
+          >
             <Grid.Column>
-              <p>
-                {farmerCompanyName ? farmerCompanyName : `${farmerFirstName} ${farmerLastName}`}
-              </p>
+              <p>{ farmerDisplayName }</p>
+              <p>{ contact.sapId }</p>
             </Grid.Column>
             <Grid.Column>
-              <p>
-                {this.props.companyName}
-              </p>
-              <p>
-                {this.props.companyBusinessId}
-              </p>
+              <p>{ companyName }</p>
+              <p>{ companyBusinessId }</p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
